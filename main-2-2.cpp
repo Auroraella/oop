12b426fc9f7335c108fff40296e565e6e282dcc6
banmaile <banmai.le@student.adelaide.edu.au>
2024-03-07 22:53:27 +1030

    Prac2

--- main-2-2.cpp
+++ main-2-2.cpp
@@ -4,10 +4,10 @@
 
 extern int binary_to_int(int binary_digits[], int number_of_digits);
 int main() {
-    int binary_digits;
     int number_of_digits;
     std::cout << "Enter the size of number: ";
     std::cin >> number_of_digits;
+    int binary_digits[number_of_digits];
     for (int i = 0; i < number_of_digits; i++) {
         std::cout << "Enter a number: ";
         std::cin >> binary_digits;
