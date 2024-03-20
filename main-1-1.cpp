8114344bd40a2c03ad633ee84ee844b6e8594c44
banmaile <banmai.le@student.adelaide.edu.au>
2024-03-20 13:49:27 +1030

    Prac3

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -7,12 +7,10 @@ int main() {
     int n = 3; 
     Person* peopleArray = createPersonArray(n);
 
-    
     for (int i = 0; i < n; i++) {
         std::cout << "Person " << i+1 << ": Name - " << peopleArray[i].name << ", Age - " << peopleArray[i].age << std::endl;
     }
 
-    
     delete[] peopleArray;
     return 0;
 }
