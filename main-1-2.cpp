d8aa4bbe547a1535f1ddf7d0f1d0c0a679d8bf46
banmaile <banmai.le@student.adelaide.edu.au>
2024-03-20 13:34:20 +1030

    Prac3

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -4,16 +4,15 @@
 extern PersonList createPersonList(int n);
 
 int main() {
-    int n = 3; // Example: Creating a PersonList with 3 Person structs
+    int n = 6;
     PersonList myList = createPersonList(n);
 
-    // Printing the details of the PersonList
+
     std::cout << "Number of people in the list: " << myList.numPeople << std::endl;
     for (int i = 0; i < myList.numPeople; i++) {
         std::cout << "Person " << i+1 << ": Name - " << myList.people[i].name << ", Age - " << myList.people[i].age << std::endl;
     }
 
-    // Don't forget to delete the dynamically allocated array to prevent memory leaks
     delete[] myList.people;
     return 0;
 }
