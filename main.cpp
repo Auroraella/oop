1b03ba69352f1863ebe2e75af3f7c769ea864b3f
Auroraella <banmaihn05@gmail.com>
2024-05-06 18:45:15 +0930

    show() implemented

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
-
+int show()
+{
+    std::cout << "This is local!";
+    std::cout << "abc" << std::endl;
+    return 0;
+}
 int main(int argc, char const *argv[])
 {
     /* code */
