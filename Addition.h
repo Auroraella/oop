fb6238f3c02fc7fc17969aa41b01d98e934a8c2e
Auroraella <banmaihn05@gmail.com>
2024-05-07 21:15:42 +0930

    Ws9

---
+++ Addition.h
@@ -0,0 +1,14 @@
+// Addition.h
+
+#ifndef ADDITION_H
+#define ADDITION_H
+
+class Addition {
+public:
+    // Function to perform addition
+    int add(int lhs, int rhs) {
+        return lhs + rhs;
+    }
+};
+
+#endif // ADDITION_H
---
+++ Invoice.h
@@ -0,0 +1,23 @@
+// Invoice.h
+
+#ifndef INVOICE_H
+#define INVOICE_H
+
+#include <string>
+
+class Invoice {
+public:
+    Invoice(std::string invoiceId);
+
+    void addServiceCost(double costDollars);
+
+    double getDollarsOwed() const;
+
+    std::string getInvoiceId() const;
+
+private:
+    std::string invoiceId;
+    double dollarsOwed;
+};
+
+#endif // INVOICE_H
